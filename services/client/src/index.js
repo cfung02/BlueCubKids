808b35fad0e15c00c470b5fee8e8fffbae415a07
Kingsley <kingsleyyliao@gmail.com>
2018-08-17 22:51:49 -0400

    Setup dev front and backend env with docker

---
+++ services/client/src/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react"; // new
+import ReactDOM from "react-dom";
+import axios from "axios";
+import UsersList from "./components/UserList";
+import AddUser from "./components/AddUser";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: [],
+      username: "work?",
+      email: ""
+    };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
+  getUsers() {
+    axios
+      .get("http://192.168.99.100/users")
+      .then(res => {
+        this.setState({ users: res.data.data.users });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios
+      .post("http://192.168.99.100/users", data)
+      .then(res => {
+        this.getUsers(); // new
+        this.setState({ username: "", email: "" }); // new
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half">
+              <br />
+              <h1 className="title is-1">All Users</h1>
+              <hr />
+              <br />
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
+              <br />
+              <br />
+              <UsersList users={this.state.users} />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
